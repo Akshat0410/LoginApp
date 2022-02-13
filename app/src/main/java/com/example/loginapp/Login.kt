4a31de3c880a710834fcb841b70d7bf994ad6c79
Akshat0410 <anupamp74@gmail.com>
2022-02-13 22:51:39 +0530

    Added all the necessary fragments

---
+++ app/src/main/java/com/example/loginapp/Login.kt
@@ -0,0 +1,17 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Login : Fragment(R.layout.fragment_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
