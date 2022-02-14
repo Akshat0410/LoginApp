902247550be3a7938a186662f9db9c064ae9c45e
Akshat0410 <anupamp74@gmail.com>
2022-02-14 22:10:13 +0530

    Navigation and Binding setup

--- app/src/main/java/com/example/loginapp/Login.kt
+++ app/src/main/java/com/example/loginapp/Login.kt
@@ -5,13 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.loginapp.databinding.FragmentLoginBinding
 
 
-class Login : Fragment(R.layout.fragment_login) {
+class Login : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get()= _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= FragmentLoginBinding.inflate(inflater,container,false)
+
+        binding.signIn.setOnClickListener {
+
+        }
+
+        binding.signUp.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_signUpFragment)
+        }
+
+        binding.googleButton.setOnClickListener {
+
+        }
 
 
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 }
--- app/src/main/java/com/example/loginapp/SignUpFragment.kt
+++ app/src/main/java/com/example/loginapp/SignUpFragment.kt
@@ -5,12 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.loginapp.databinding.FragmentSignUpBinding
 
 
-class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+class SignUpFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        binding.signIn.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_login2)
+        }
+
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
